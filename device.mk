36a743d0709a6b496a7b6cfddc6e3b68edf2ecf4
mike dooley <mdooley@google.com>
2019-12-15 14:16:04 +0600

    markw: Enabling sound trigger V2_2
    V2_2 has the same functionality as V2_1 except it adds one new
    api, getModelState.
    
    Note: V2_1 will be removed in a subsequent cl.
    
    Test: built android and tested using SoundTriggerTestApp
    
    Bug: 70206501
    Change-Id: Id2647687732e79df252588d69f95d2a407e0092c
    Signed-off-by: ShihabZzz <shihab.riadn@gmail.com>

--- device.mk
+++ device.mk
@@ -91,7 +91,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.primary.msm8953 \
     audio_amplifier.msm8953 \
